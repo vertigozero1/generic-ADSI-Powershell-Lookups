f429cb9ae84081622dac0588ba2cfcc18895720b
Stiles, Jason <Jason.Stiles@parsons.com>
2023-03-23 15:46:28 -0500

    Added managedobjects.count to defaultProperties

--- adUserLookup.ps1
+++ adUserLookup.ps1
@@ -279,7 +279,7 @@ PROCESS {
     #Prepare and execute primary query
     $searcher = ([adsisearcher]"(&(objectCategory=Person)(objectClass=User))")
     if ($defaultProperties) {
-        $properties = "samaccountname", "name", "employeetype", "usperson", "company", "title", "manager", "userprincipalname", "co", "physicaldeliveryofficename", "firstworkingday", "whencreated", "pwdlastset", "directreports", "memberof"
+        $properties = "samaccountname", "name", "employeetype", "usperson", "company", "title", "manager", "userprincipalname", "co", "physicaldeliveryofficename", "firstworkingday", "whencreated", "pwdlastset", "directreports", "memberof", "managedobjects"
         foreach ($property in $properties) { $searcher.PropertiesToLoad.Add($property) | out-null }
     }
 
@@ -448,26 +448,27 @@ END {
                 catch { $fmName = $fmDN }
 
                 $tempObj = [pscustomobject] @{
-                    ID            = $user.samaccountname -join ";"
-                    Name          = $user.name -join ";"
-                    Status        = $user.employeetype -join ";"
-                    USPerson      = $user.usperson -join ";"
-                    BU            = $user.company -join ";"
-                    Title         = $user.title -join ";"
-                    Manager       = $fmName
-                    Email         = $user.userprincipalname -join ";"
-                    Country       = $user.co -join ";"
-                    Office        = $user.physicaldeliveryofficename -join ";"
-                    StartDate     = $user.firstworkingday -join ";"
-                    PwdLastSet    = [datetime]::FromFileTime($user.pwdlastset[0])
-                    DirectReports = $user.directreports.count
-                    MemberOf      = $user.memberof.count
+                    ID             = $user.samaccountname -join ";"
+                    Name           = $user.name -join ";"
+                    Status         = $user.employeetype -join ";"
+                    USPerson       = $user.usperson -join ";"
+                    BU             = $user.company -join ";"
+                    Title          = $user.title -join ";"
+                    Manager        = $fmName
+                    Email          = $user.userprincipalname -join ";"
+                    Country        = $user.co -join ";"
+                    Office         = $user.physicaldeliveryofficename -join ";"
+                    StartDate      = $user.firstworkingday -join ";"
+                    PwdLastSet     = [datetime]::FromFileTime($user.pwdlastset[0])
+                    DirectReports  = $user.directreports.count
+                    MemberOf       = $user.memberof.count
+                    ManagedObjects = $user.managedobjects.count
                 }
                 $tempObj
             }
             $result = $tempResult
-            if ($export) { Write-Output $result | Select-Object ID, Name, Status, USPerson, Title, Manager, Email, Country, Office, StartDate, PwdLastSet, DirectReports, MemberOf | Sort-Object ID | Out-File -FilePath $targetFile -Append }
-            else { Write-Output $result | Select-Object ID, Name, Status, USPerson, Title, Manager, Email, Country, Office, StartDate, PwdLastSet, DirectReports, MemberOf | Sort-Object ID }
+            if ($export) { Write-Output $result | Select-Object ID, Name, Status, USPerson, Title, Manager, Email, Country, Office, StartDate, PwdLastSet, DirectReports, MemberOf, ManagedObjects | Sort-Object ID | Out-File -FilePath $targetFile -Append }
+            else { Write-Output $result | Select-Object ID, Name, Status, USPerson, Title, Manager, Email, Country, Office, StartDate, PwdLastSet, DirectReports, MemberOf, ManagedObjects | Sort-Object ID }
         }
         else { $result | Sort-Object | Format-Table -AutoSize }
     }
